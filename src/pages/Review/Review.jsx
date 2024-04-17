0b5980e051dd2698d4285d6d359f2dd90d59ceed
khadiza21 <bibikhadiza474@gmail.com>
2024-04-18 01:38:56 +0600

    reviews section working process continue...

--- src/pages/Review/Review.jsx
+++ src/pages/Review/Review.jsx
@@ -3,16 +3,20 @@
 const Review = ({ item }) => {
     const { id, name, img, review, star, } = item;
     return (
-     
-           <div className="">
-                <img className="w-[120px]" src={img} />
+
+        <div className="bg-white shadow-lg rounded-lg p-4">
+            <div className="flex items-center mb-4">
+                <img src={img} alt="Reviewer" className="rounded-full w-12 h-12 mr-4" />
                 <div>
-                    <h3 className="uppercase">{name}</h3>
-                    <h5>Review: {review}</h5>
+                    <h3 className="font-semibold">{name}</h3>
+                   
                     <h4>Star: {star}</h4>
                 </div>
-           </div>
-      
+            </div>
+
+             <p className="text-gray-800">{review}</p>
+        </div>
+
     );
 };
 
