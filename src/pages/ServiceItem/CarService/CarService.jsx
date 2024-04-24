81737f837beec9bb8b387b4003458ce99980e1c6
khadiza21 <bibikhadiza474@gmail.com>
2024-04-24 14:29:57 +0600

    car service page banner update

--- src/pages/ServiceItem/CarService/CarService.jsx
+++ src/pages/ServiceItem/CarService/CarService.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import caBanner from '../../../assets/car.jpeg';
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
 const CarService = () => {
     return (
         <div>
             <Helmet>
                 <title>City Mover | Car Service</title>
             </Helmet>
-            <h3>Car service</h3>
+            <Cover img={caBanner} title="Car services"></Cover>
+
+          
         </div>
     );
 };
