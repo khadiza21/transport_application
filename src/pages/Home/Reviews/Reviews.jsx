2995fdc40dd42d09e2675a39bb666958ee49d74a
khadiza21 <bibikhadiza474@gmail.com>
2024-04-30 19:27:34 +0600

    update

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -11,7 +11,7 @@ import '@smastrom/react-rating/style.css'
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
-        fetch('review.json')
+        fetch('http://localhost:5000/review')
             .then(res => res.json())
             .then(data => {
                 setReviews(data)
