ace3d2c79419a0ddbb323c80aebafaa6159e2636
khadiza21 <bibikhadiza474@gmail.com>
2024-05-01 15:33:18 +0600

    login from update

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,73 +1,95 @@
+import { useState } from 'react';
 import loginimg from '../../../assets/log2.webp';
 
 const Login = () => {
+    const currentYear = new Date().getFullYear();
+
+    const [selectedValue, setSelectedValue] = useState();
+
+    const handleSelectChange = (event) => {
+        setSelectedValue(event.target.value);
+    }
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const form = event.email.value;
+        
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
+
+  
     return (
-        <div className=' my-24 px-10'>
-              <h1 className='text-center font-bold uppercase text-4xl py-8 '>Sign Up</h1>
-            <div className="grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2  ">
-                <div>
+        <div className=' my-24 px-44 mx-48 sm:px-8 md:py-  '>
+            <h1 className='text-center font-bold uppercase text-4xl py-8 '>Sign Up</h1>
+            <div className="grid sm:grid-cols-1 lg:grid-cols-2 md:grid-cols-2 ">
+                <div className=' flex justify-center items-center'>
                     <figure><img src={loginimg} alt="authentication-login" /></figure>
                 </div>
 
+                <div className='flex justify-center items-center'>
+                    <div className="card shrink-0 w-full  shadow-2xl bg-base-100  ">
+                        <form className="card-body" onSubmit={handleLogin}>
+                            <div className="form-control">
 
 
-                <div className="bg-slate-200 p-10 rounded-lg shadow-xl">
-                  
-                    <form class="max-w-sm mx-auto">
-                        <div class="mb-5">
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Your email</label>
-                            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-                        </div>
-                        <div class="mb-5">
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">Your password</label>
-                            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                        </div>
-                        <div class="flex items-start mb-5">
-                            <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                            </div>
-                            <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-dark-300">Remember me</label>
-                        </div>
-                        <button type="submit" class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">Submit</button>
-                    </form>
-
-                </div>
-
-
-
-                <div class="w-full max-w-xs">
-                    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        <div class="mb-4">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                                Username
-                            </label>
-
-                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" placeholder="Username" />
-                        </div>
-                        <div class="mb-6">
-                            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                                Password
-                            </label>
-                            <input  class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
                            
-                                <p class="text-red-500 text-xs italic">Please choose a password.</p>
-                        </div>
-                        
-                        <div class="flex items-center justify-between">
-                            <button class="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                                Sign In
-                            </button>
-                            <a class="inline-block align-baseline font-bold text-sm text-yellow-600 hover:text-yellow-700" href="#">
-                                Forgot Password?
-                            </a>
-                        </div>
-                    </form>
-                    <p class="text-center text-gray-500 text-xs">
-                        &copy;2020 Acme Corp. All rights reserved.
-                    </p>
+
+                         
+                                <label className="label dropdown ">
+                                    <span className="label-text font-bold"> Sign Up As  </span>
+                                </label>
+                                <div className='input input-bordered'>
+                                    <select id="dropdown" value={selectedValue} onChange={handleSelectChange} className='border-0 mt-2 border-none outline-none'>
+                                        <option className=" text-gray-400" value="">Select...</option>
+                                        <option className=""  value="As an Admin">As an Admin</option>
+                                        <option className=""  value="As a User">As a User</option>
+                                        <option className=""  value="As a Driver">As a Driver</option>
+                                    </select>
+                                    
+                                </div>
+
+
+
+                                <label className="label">
+                                    <span className="label-text font-bold">  Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+
+                                <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                                <div className="flex items-start my-5">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                    </div>
+                                    <label for="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-dark-300">Remember me</label>
+                                </div>
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover align-baseline font-bold text-sm text-dark-600 hover:text-yellow-800">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input className='btn bg-yellow-600 hover:bg-yellow-700 text-white font-bold' type="submit" value="Sign Up" />
+                            </div>
+
+                        </form>
+
+                    </div>
                 </div>
+
 
             </div>
+
+            <p className="text-center text-gray-500 text-xs font-bold my-24">
+                &copy;{currentYear} bk group. All rights reserved.
+            </p>
         </div>
     );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Navbar.css';
 
 const Navbar = () => {
 
     const navOptions = <>
 
         <li> <Link to="/home"> <span className='text-white'>Home</span></Link></li>
-        <li className='dropdownList   '>
-            <details className='  ' >
-                <summary className=''><span className='text-white '>Services</span></summary>
-                <ul className="p-2 bg-opacity-50 bg-neutral-950 ">
+        <li>
+            <details>
+                <summary><span className='text-white'>Services</span></summary>
+                <ul className="p-2 bg-opacity-50 bg-neutral-950">
                     <li><Link to="/busService"><span className='text-white'>Bus</span></Link></li>
                     <li><Link to="/carService"><span className='text-white'>Car</span></Link></li>
                     <li><Link to="/bikeService"><span className='text-white'>Bike</span></Link></li>
@@ -19,9 +18,9 @@ const Navbar = () => {
             </details>
         </li>
         <li>
-            <details className=' '>
-                <summary><span className='text-white '>Earn</span></summary>
-                <ul className="p-2 bg-opacity-50 bg-neutral-950 ">
+            <details>
+                <summary><span className='text-white'>Earn</span></summary>
+                <ul className="p-2 bg-opacity-50 bg-neutral-950">
                     <li><a><span className='text-white'>Bus</span></a></li>
                     <li><a><span className='text-white'>Bike</span></a></li>
                     <li><a><span className='text-white'>Car</span></a></li>
@@ -29,16 +28,16 @@ const Navbar = () => {
             </details>
         </li>
         <li><Link to="/about"><span className='text-white'>About</span></Link></li>
-        <li><Link to="/contact"><span className='text-white'>Contact</span></Link></li>
+        <li><Link to="/contact"><span className='text-white'></span>Contact</Link></li>
 
     </>
 
     return (
         // px-48 is appropriet for large device. for small device not appropriet. need to responsive bg-base-100
-        <div className="navbar fixed z-10 bg-opacity-50  bg-neutral-950 text-white px-40">
-            <div className="navbar-start   mx-auto  ">
-                <div className="dropdown ">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
+        <div className= "navbar fixed z-10 bg-opacity-50  bg-neutral-950 text-white px-40">
+            <div className="navbar-start   mx-auto ">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className=" font-bold menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-white rounded-box w-48  bg-slate-900 font-bold px-0">
@@ -47,13 +46,13 @@ const Navbar = () => {
                 </div>
                 <Link to="/" className=" py-2  px-2 text-xl font-bold "> <span className='text-[26px] font-bold '>  <span className='px-0 mx-0 text-yellow-500  font-bold text-[28px]'>City</span >Mover</span></Link>
             </div>
-            <div className="navbar-center hidden lg:flex ">
-                <ul className="menu menu-horizontal px-1 text-white font-bold px-48  ">
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-white font-bold px-48 ">
                     {navOptions}
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link className='btn font-bold' to="/login">Sign Up</Link>
+            <Link className='btn font-bold' to="/login">Sign Up</Link>
             </div>
         </div>
 
